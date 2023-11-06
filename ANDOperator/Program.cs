8ee962c3c137f41d4bc9b21e42e9926b5ffabac6
Imre Kli. <imre.koli@tktk.ee>
2023-11-06 11:12:38 +0200

    add week 5 files

---
+++ ANDOperator/Program.cs
@@ -0,0 +1,24 @@
+﻿//Math >=90; Biology >=90, Chemistry >=90;
+
+using System.Transactions;
+
+int math, biology, chemistry;
+
+Console.WriteLine("Enter your math result");
+math = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter your biology result");
+biology = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter your chemistry result");
+chemistry = Int32.Parse(Console.ReadLine());
+
+
+if (math >= 90 && biology >= 90 && chemistry >= 90)
+{
+    Console.WriteLine("Congratulation! You got accepted");
+}
+else
+{
+    Console.WriteLine("Your application cannot be approved");
+}
---
+++ Week5LogicalOperators/Program.cs
@@ -0,0 +1,18 @@
+﻿
+
+
+//AND &&
+
+//TRUE &&  ture --> true
+//true && false --> true
+//false && true --> false
+//false && false --> false
+
+//OR - || (pipes)
+
+
+//true ||  true --> true
+//true ||  false --> true
+//false || true --> true
+//false || false --> false
+
